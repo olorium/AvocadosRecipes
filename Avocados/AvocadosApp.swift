2bf70293e0d9f9432bb3bc4346e7c77b311d0761
Alex Vasyliev <olorin2007@gmail.com>
2023-05-08 20:18:03 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Oleksii Vasyliev on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
