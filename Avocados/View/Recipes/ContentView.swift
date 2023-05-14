25188dab2e0f62f6a8b3d20430e7a79fa16a18b0
Alex Vasyliev <olorin2007@gmail.com>
2023-05-14 15:34:01 +0200

    add content for the recipes screen

---
+++ Avocados/View/Recipes/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Oleksii Vasyliev on 08.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		ScrollView(.vertical, showsIndicators: false) {
+			VStack(alignment: .center, spacing: 20) {
+				// MARK: - Header
+				ScrollView(.horizontal, showsIndicators: false) {
+					HStack(alignment: .top, spacing: 0) {
+						HeaderView()
+					}
+				}
+				
+				// MARK: - Footer
+				VStack(alignment: .center, spacing: 20) {
+					Text("All About Avocados")
+						.font(.system(.title, design: .serif))
+						.fontWeight(.bold)
+						.foregroundColor(Color("ColorGreenAdaptive"))
+						.padding(8)
+					
+					Text("Everything you wanted to know about avocados but were too afraid to ask.")
+						.font(.system(.body, design: .serif))
+						.multilineTextAlignment(.center)
+						.foregroundColor(.gray)
+					
+				}
+				.frame(maxWidth: 640)
+				.padding()
+				.padding(.bottom, 85)
+			}
+		}
+		.edgesIgnoringSafeArea(.all)
+		.padding(0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
